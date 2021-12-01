b21d428dfb474cb4b4a7de86e3e0d302a2ecc1d1
jcruzer <jcruzer@naver.com>
2021-12-01 19:30:40 +0900

    21-12-01 practice

--- js/main.js
+++ js/main.js
@@ -42,3 +42,42 @@ fadeEls.forEach(function(fadeEl, index){
     delay:(index + 1) * .7  // 딜레이로 0.7초 1.4초 2.1초 2.7초뒤 순차적 등장
   });
 });
+
+new Swiper('.notice-line .swiper.container', {
+  direction:'vertical',
+  autoplay:true,
+  loop:true
+});
+
+new Swiper('.promotion .swiper.container', {
+  direction:'horizontal',
+  slidesPerView:3,
+  spaceBetween: 10,
+  centeredSlides:true,
+  loop:true,
+  autoplay:{
+    delay:5000
+  },
+  pagination:{
+    el:'.promotion .swiper-pagination',
+    clickable:true
+  },
+  navigation:{
+    prevEl:'.promotion .swiper-prev',
+    nextEl:'.promotion .swiper-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click',function(){
+  isHidePromotion = !isHidePromotion;
+  if(isHidePromotion){
+    promotionEl.classList.add('hide');
+  }
+  else{
+    promotionEl.classList.remove('hide');
+  }
+
+});
